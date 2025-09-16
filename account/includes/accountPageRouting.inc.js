75fe1c49d6982789cf68ca82a7dea9cd3ef991a5
titus <titustesche@outlook.de>
2025-09-16 21:34:40 +0200

    This now loads content dynamically from available pages based on the ?page parameter

--- account/includes/accountPageRouting.inc.js
+++ account/includes/accountPageRouting.inc.js
@@ -1,19 +1,20 @@
-window.onload = () => {
+window.onload = async () => {
     const params = new URLSearchParams(window.location.search);
+    const wrapper = document.getElementById('content-wrapper');
+    let content;
 
-    // Load different body elements for whatever you wish to display
-    switch (params.get('page')) {
-        case 'register':
-            alert("Register");
-            break;
-        case 'login':
-            alert("Login");
-            break;
-        case 'resetPassword':
-            alert("Reset Password");
-            break;
-        case 'logout':
-            alert("Logout");
-            break;
-    }
+    // Load different body elements based on the parameters in the URL
+    await fetch(`includes/${params.get('page')}.html`)
+        .then(response => {
+            // If the response returned 200, pass the received HTML
+            if (response.ok) return response.text();
+            // If not, pass and error message
+            return `<h1>The requested content could not be loaded! <br />Are the parameters correct?</h1>`;
+        })
+        .then(res => {
+            // Assign error message to the res (yes this needs to be in its own .then because otherwise it will be a pending promise)
+            content = res;
+        })
+    // Display the loaded content
+    wrapper.innerHTML = content;
 }
