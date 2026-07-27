061fd52887309918094c1c473ab09375d2f9f156
titus <titustesche@outlook.de>
2026-07-27 13:02:43 +0200

    feat: add debug mode
    add option for debug mode to be globally enabled

--- frontendv2/Managers/SettingsManager.js
+++ frontendv2/Managers/SettingsManager.js
@@ -1,6 +1,13 @@
 export class SettingsManager {
     static accentColor = undefined;
+    static _debugMode;
     static providerUrls = [];
+
+    static get debugMode() { return SettingsManager._debugMode; }
+    static set debugMode(value) {
+        localStorage.setItem("debug-mode", value);
+        SettingsManager._debugMode = value;
+    }
 
     static setAccentColor(color) {
         SettingsManager.accentColor = color;
@@ -16,9 +23,11 @@ export class SettingsManager {
     static loadSettings() {
         const accentColor = localStorage.getItem("accent-color");
         const providerUrls = localStorage.getItem("provider-urls");
+        const debugMode = localStorage.getItem("debug-mode") ?? false;
 
         if (accentColor) SettingsManager.accentColor = accentColor;
         if (providerUrls) SettingsManager.providerUrls = JSON.parse(providerUrls);
+        SettingsManager._debugMode = debugMode;
 
         SettingsManager.applySettings();
     }
