70e70606c63c4b2265f463fa2b2d7e5589d7847a
titus <titustesche@outlook.de>
2026-08-05 23:24:55 +0200

    feat: add handling for connection identification, sending messages and interpreting notifications from the backend; increase ping interval to 10s

--- frontendv2/Managers/WebSocketManager.js
+++ frontendv2/Managers/WebSocketManager.js
@@ -1,5 +1,6 @@
-import {SettingsManager} from "./SettingsManager.js";
 import {API_ADAPTER} from "../api-adapter.js";
+import {Popup} from "../Components/Popup.js";
+import {ConversationManager} from "./ConversationManager.js";
 
 export class WebSocketManager {
     static _socket;
@@ -9,14 +10,22 @@ export class WebSocketManager {
     static _lastPong;
     static _pingInterval;
 
+    // Todo: Keep identifiers to make reconnect more persistent?
+    static _identity;
+    static get identity() { return this._identity; }
+    static set identity(value) {
+        this._identity = value;
+        if (this._socket) this._socket.send(JSON.stringify({type: "identification", identifier: value}));
+    }
+
     static get socket() { return this._socket; }
     static get isConnected() { return this._isConnected; }
 
     static get ping() { return this._lastPong - this._lastPing; }
 
-    static ping = () => {
+    static sendPing = () => {
         if (this._socket && this._socket.readyState === WebSocket.OPEN) {
-            this._socket.send(JSON.stringify({type: "ping"}));
+            this._socket.send(JSON.stringify({identity: this.identity, type: "ping"}));
             this._lastPing = Date.now();
         }
     }
@@ -27,8 +36,7 @@ export class WebSocketManager {
         this._socket.onopen = () => {
             this._isConnected = true;
             this._lastPing = Date.now();
-            this._pingInterval = setInterval(this.ping, 1000);
-            this._socket.send("Hello from client!");
+            this._pingInterval = setInterval(this.sendPing, 10000);
         };
 
         this._socket.onclose = () => {
@@ -39,11 +47,37 @@ export class WebSocketManager {
             }
         };
 
-        this._socket.onmessage = (event) => {
+        this._socket.onmessage = async (event) => {
             const data = JSON.parse(event.data);
-            if (data.type === "pong") {
-                this._lastPong = Date.now();
+
+            switch (data.type) {
+                case "identification":
+                    this.identity = data.body;
+                    break;
+
+                case "pong":
+                    this._lastPong = Date.now();
+                    console.log(`Ping: ${this._lastPong - this._lastPing}ms`);
+                    break;
+
+                case "notification":
+                    Popup.info(data.body.title, data.body.body);
+                    break;
+
+                case "messageUpdate":
+                    await ConversationManager.UpdateMessage(data.body);
+                    break;
+
+                case "messageCreated":
+                    await ConversationManager.AddMessage(data.body);
+                    break;
             }
         };
+    }
+
+    static send(type, body) {
+        if (this._socket && this._socket.readyState === WebSocket.OPEN) {
+            this._socket.send(JSON.stringify({identity: this.identity, type, body}));
+        }
     }
 }
